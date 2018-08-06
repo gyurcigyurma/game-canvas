39106f72f9d1e43360475676a0625d69f955e09e
Gyorgy Raffay <Gyorgy_Raffay@epam.com>
2018-08-06 13:44:55 +0200

    error

--- src/helpers.js
+++ src/helpers.js
@@ -1,10 +1,10 @@
-function randomColor(){
+ function randomColor(){
     const colorsGenerated = randomNum();
     let {color1, color2, color3, alpha} = colorsGenerated;
     return `rgba(${color1}, ${color2}, ${color3}, ${alpha})`
 }
 
-function randomNum () {
+ function randomNum () {
     return {
         color1: (Math.random()*255).toFixed(0),
         color2: (Math.random()*255).toFixed(0),
@@ -13,4 +13,11 @@ function randomNum () {
     }
 }
 
-export { randomColor, randomNum };
+ function getCursorPosition(canvas, event) {
+    var rect = canvas.getBoundingClientRect();
+    var x = event.clientX - rect.left;
+    var y = event.clientY - rect.top;
+    console.log("x: " + x + " y: " + y);
+}
+
+export { randomColor, randomNum, getCursorPosition };
--- src/main.js
+++ src/main.js
@@ -4,52 +4,50 @@ const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
 const text ='Nándi bácsi';
 
+const canvasWidth = canvas.width;
+const canvasHeight = canvas.height;
+
 let degree= 0;
-function drawRectanle() {
+function drawRectanle(x,y,width,height) {
     
-//    context.clearRect(0, 0, context.canvas.width, context.canvas.height);
-//    
-//    context.translate(context.canvas.width/2, context.canvas.height/2);
-//    context.rotate(degree * Math.PI * 180);
-//    
-//    context.translate(-context.canvas.width/2, -context.canvas.height/2);
-//    
-//    context.strokeRect(100,100,200,100);
-//    
- degree = (degree + 1) % 360;
-    context.clearRect(0, 0, canvas.width, canvas.height);
-
-    context.save();
-    context.fillStyle = "#FF0000";
-
-    context.translate(200,150);
-    context.rotate( degree*Math.PI/180 ); 
-    context.translate(-200,-150);
-
-    context.beginPath();
-    context.strokeRect(100,100,200,100)
-
-    context.restore();
-
-    
-
+  const centerPoint = findCenter(x,y,width,height);    
+  //context.clearRect(0, 0, canvasWidth, canvasHeight);
+  //context.fillStyle(randomColor());
+  context.save();
+  context.translate(centerPoint.xpos, centerPoint.ypos);
+  context.rotate(degree * Math.PI / 180);
+  context.translate(-centerPoint.xpos, -centerPoint.ypos);
+  degree++;
+  context.strokeRect(x,y, width, height);
+  context.restore();
 
 
 }
 
-function drawLines(){
-    context.beginPath();
-    context.lineWidth = Math.random()*5;
-    context.moveTo(Math.floor(Math.random()*context.context.canvas.width), Math.floor(Math.random()*context.context.canvas.height));
-    context.lineTo(Math.floor(Math.random()*context.context.canvas.width), Math.floor(Math.random()*context.context.canvas.height))
-    
-    context.strokeStyle = randomColor();
-    context.stroke();
+function findCenter (x,y,width,height) {
+    return {
+        xpos: x + width/2,
+        ypos: y + height/2
+    }
 }
 
+//function drawLines(){
+//    context.beginPath();
+//    context.lineWidth = Math.random()*5;
+//    context.moveTo(Math.floor(Math.random()*context.context.canvas.width), Math.floor(Math.random()*context.context.canvas.height));
+//    context.lineTo(Math.floor(Math.random()*context.context.canvas.width), Math.floor(Math.random()*context.context.canvas.height))
+//    
+//    context.strokeStyle = randomColor();
+//    context.stroke();
+//}
 
-setInterval(drawRectanle, 30)
 
+//setInterval(drawRectanle.bind(this, 500, 100, 200,100), 55, this);
+
+//setInterval(drawRectanle.bind(this, 100, 200, 100,400), 55);
+
+drawRectanle(10,10,100,120)
+//canvas.addEventListener('click', getCursorPosition.bind(null, canvas))
 
 
 //drawRectanle();
